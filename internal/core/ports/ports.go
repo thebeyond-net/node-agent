c7a3b33caf25133bd0421d23da0fca6ea8ab14f6
クロナミ Abyss <joibelitz374@hotmail.com>
2026-02-28 22:10:04 +0300

    ✨ feat(core): rename amneziawg to peer, add ip allocation and vpn provider interfaces

--- internal/core/ports/ports.go
+++ internal/core/ports/ports.go
@@ -1,8 +1,27 @@
 package ports
 
-import "context"
+import (
+	"context"
+	"net/netip"
 
-type AmneziaWG interface {
-	CreatePeer(ctx context.Context) (clientConf, clientPubKey string, err error)
+	"github.com/thebeyond-net/node-agent/internal/core/domain"
+)
+
+type VPNProvider interface {
+	GetServerConfig() (domain.ServerConfig, error)
+	RegisterPeer(pubKey, allowedIP string) error
+	RemovePeer(pubKey string) error
+	BuildClientConfig(privKey, address string, srv domain.ServerConfig) (string, error)
+}
+
+type IPAllocationRepository interface {
+	Allocate(ctx context.Context, prefix netip.Prefix) (netip.Addr, error)
+	Reserve(ctx context.Context, ip netip.Addr, publicKey string, prefix netip.Prefix) error
+	Release(ctx context.Context, ip netip.Addr) error
+	ReleaseByPublicKey(ctx context.Context, publicKey string) error
+}
+
+type PeerUseCase interface {
+	CreatePeer(ctx context.Context) (clientConf string, pubKey string, err error)
 	DeletePeer(ctx context.Context, pubKey string) error
 }

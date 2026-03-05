a30ee58bf0e48064a03458b0a5d1720847a1f4a7
クロナミ Abyss <joibelitz374@hotmail.com>
2026-03-05 22:40:56 +0300

    🚚 refactor: move config to separate package and add networking fields

--- config/config.go
+++ config/config.go
@@ -1,4 +1,4 @@
-package main
+package config
 
 import (
 	"fmt"
@@ -9,11 +9,15 @@ import (
 type Config struct {
 	MigrationsPath string `env:"MIGRATIONS_PATH" env-default:"./migrations"`
 	DatabasePath   string `env:"DATABASE_PATH" env-default:"./data/ips.db"`
-	Port           string `env:"PORT" env-default:"2000"`
+	Port           string `env:"PORT" env-default:"4080"`
 	LogLevel       string `env:"LOG_LEVEL" env-default:"info"`
+	NodeIP         string `env:"NODE_IP" env-required:"true"`
+	NetworkCIDR    string `env:"NETWORK_CIDR" env-default:"10.10.0.0/20"`
+	ClientDNS      string `env:"CLIENT_DNS" env-default:"1.1.1.1,8.8.8.8"`
+	AuthSecret     string `env:"AUTH_SECRET" env-required:"true"`
 }
 
-func LoadConfig() (*Config, error) {
+func Load() (*Config, error) {
 	var cfg Config
 	if err := cleanenv.ReadConfig(".env", &cfg); err != nil {
 		return nil, fmt.Errorf("config error: %w", err)
